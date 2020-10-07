ff989476387406eb9484cf25925722f0c112b915
Sergio Duran Perez <32856603+checotomas@users.noreply.github.com>
2020-10-07 11:31:28 -0500

    Add files via upload

---
+++ AOI/Conexion.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	$servidor="34.66.142.177";
+	$usuario="sergio";
+	$contraseña="sergio";
+	$basedatos="aoi_remote";
+	$conn=mysqli_connect("$servidor","$usuario","$contraseña","$basedatos");
+	if (mysqli_connect_errno()){
+  		echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
+  	}
+  	echo "Listo bd";
+  	*/
+  	$dns="/cloudsql/proyecto-pruebas-291115:us-central1:prueba;dbname=prueba";
+  	$user="root";
+  	$pass="root";
+  	$db = new PDO($dsn, $user, $pass);
+  	echo $db;
+  	//mysql:"host=127.0.0.1";port="3306";unix_socket="/cloudsql/<Project ID>:<Location>:<Instance>;dbname=<Database Name>"
+?>
