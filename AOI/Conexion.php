42b51c62ed724ea2de05755e8147dda4f1bdf3ed
Sergio Duran Perez <32856603+checotomas@users.noreply.github.com>
2020-10-07 01:04:28 -0500

    Add files via upload

---
+++ AOI/Conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+	$servidor="35.193.62.172";
+	$usuario="Sergio";
+	$contraseña="Sergio";
+	$basedatos="aoi_remote";
+	$conn=mysqli_connect("$servidor","$usuario","$contraseña","$basedatos");
+	if (mysqli_connect_errno()){
+  		echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
+  	}
+  	echo "Listo bd";
+
+
+  	
+?>
